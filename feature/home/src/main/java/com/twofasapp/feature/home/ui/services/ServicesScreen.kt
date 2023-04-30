71f23f5e9fba0ecfc23d57486b1801109e4f1fb1
Rafał Kobyłko <rafakob@gmail.com>
2023-04-30 12:40:07 +0200

    Fix focus requester not init

--- feature/home/src/main/java/com/twofasapp/feature/home/ui/services/ServicesScreen.kt
+++ feature/home/src/main/java/com/twofasapp/feature/home/ui/services/ServicesScreen.kt
@@ -74,6 +74,7 @@ import com.twofasapp.feature.home.ui.services.modal.AddServiceModal
 import com.twofasapp.feature.home.ui.services.modal.FocusServiceModal
 import com.twofasapp.feature.home.ui.services.modal.ModalType
 import com.twofasapp.locale.TwLocale
+import kotlinx.coroutines.android.awaitFrame
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import org.burnoutcrew.reorderable.ReorderableItem
@@ -218,6 +219,7 @@ private fun ServicesScreen(
 
     LaunchedEffect(Unit) {
         if (uiState.searchFocused) {
+            awaitFrame()
             focusRequester.requestFocus()
         }
     }
