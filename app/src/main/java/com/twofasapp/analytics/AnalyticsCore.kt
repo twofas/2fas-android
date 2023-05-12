ba4614420141841d65c982c6ef9756a2dbe05446
Rafał Kobyłko <rafakob@gmail.com>
2023-05-12 14:58:30 +0200

    Prase services json

--- app/src/main/java/com/twofasapp/analytics/AnalyticsCore.kt
+++ app/src/main/java/com/twofasapp/analytics/AnalyticsCore.kt
@@ -1,5 +1,6 @@
 package com.twofasapp.analytics
 
+import com.google.firebase.crashlytics.FirebaseCrashlytics
 import com.twofasapp.common.analytics.Analytics
 import com.twofasapp.common.analytics.AnalyticsEvent
 import com.twofasapp.common.analytics.AnalyticsParam
@@ -7,6 +8,7 @@ import com.twofasapp.common.analytics.AnalyticsParam
 class AnalyticsCore : Analytics {
 
     override fun captureException(exception: Throwable?) {
+        exception?.let { FirebaseCrashlytics.getInstance().recordException(it) }
     }
 
     override fun captureEvent(event: AnalyticsEvent, vararg params: Pair<AnalyticsParam, String?>) {
