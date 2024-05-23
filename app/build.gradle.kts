03284f0d998ac494ab84463ef704a8b3ae62c7d9
Rafał Kobyłko <rafakob@gmail.com>
2024-05-23 15:01:23 +0200

    Bump version 5.4.5 (5000021)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,7 +18,7 @@ android {
     defaultConfig {
         applicationId = "com.twofasapp"
         versionName = "5.4.5"
-        versionCode = 5000020
+        versionCode = 5000021
 
         val versionCodeOffset = 5000000
 
